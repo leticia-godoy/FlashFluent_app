4f0ee822005fed82be3bbb599ecb4dc837d44b2b
Letícia Godoy <85085064+leticia-godoy@users.noreply.github.com>
2023-10-21 15:18:42 -0300

    Added: delete users and all dependencies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user =  User.find(params[:id])
+        @user.destroy        
+        flash[:success] = "Usuário deletado com sucesso!"
+        redirect_to users_path
+    end 
+
     private
 
     def user_params
