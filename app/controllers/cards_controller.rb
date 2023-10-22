b93776b8de323e4d8db02326a5bf999e9b48d0d3
Letícia Godoy <85085064+leticia-godoy@users.noreply.github.com>
2023-10-22 15:21:16 -0300

    Added: Instructors toggle

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,6 +3,7 @@ class CardsController < ApplicationController
     before_action :set_card, only: [:show, :edit, :update, :destroy]
     before_action :require_user, except: [:index, :show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_instructor, only: [:new, :create, :edit, :update, :destroy]
 
     def index
         @cards = Card.paginate(page: params[:page], per_page: 5)
@@ -60,6 +61,13 @@ class CardsController < ApplicationController
     def require_same_user
         if current_user != @card.user
             flash[:danger] = "Você só pode editar/deletar os seus flashCards"
+            redirect_to cards_path
+        end
+    end
+
+    def require_instructor
+        unless current_user.instructor?
+            flash[:danger] = "Apenas instrutores podem criar ou editar flashcards."
             redirect_to cards_path
         end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_021407) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_180041) do
   create_table "cards", force: :cascade do |t|
     t.string "word"
     t.text "description"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_021407) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "instructor", default: false
   end
 
 end
