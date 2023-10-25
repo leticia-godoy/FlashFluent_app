c5d4672d01953ea0a6564c909daa336206d9e90a
Letícia Godoy <85085064+leticia-godoy@users.noreply.github.com>
2023-10-25 00:06:37 -0300

    Added: topics edit/new pages

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,20 +1,29 @@
 class TopicsController < ApplicationController
 
     before_action :set_topic, only: [:edit,:update,:show]
-    def new
+    before_action :require_instructor, except: [:show, :index]
 
+    def new
+        @topic = Topic.new
     end
 
     def create
-
-    end
-
-    def edit
-
+        @topic = Topic.new(topic_params)
+        if @topic.save
+            flash[:success] = "Tópico criado com sucesso!"
+            redirect_to topic_path(@topic)
+        else
+            render 'new'
+        end
     end
 
     def update
-
+        if @topic.update(topic_params)
+            flash[:success] = "Tópico atualizado com sucesso!"
+            redirect_to @topic
+        else
+            render 'edit'
+        end
     end
 
     def show
@@ -27,7 +36,18 @@ class TopicsController < ApplicationController
     
     private
 
+    def topic_params
+        params.require(:topic).permit(:name)
+    end
+
     def set_topic 
         @topic = Topic.find(params[:id])
+    end
+
+    def require_instructor
+        if !logged_in? || (logged_in? and !current_user.instructor?)
+            flash[:danger] = "Apenas instrutores podem realizar esta ação"
+            redirect_to topics_path
+        end
     end
 end
